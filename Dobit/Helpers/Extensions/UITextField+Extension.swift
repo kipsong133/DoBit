ffd8e941dc49bd5a0244a4a52a35009b0ab46a4a
gusdn5387 <gusdn5387@naver.com>
2022-08-05 14:52:56 +0900

    회원가입, 로그인, 비밀번호 재설정 > textfield border 및 구분선 색 변경

--- Dobit/Helpers/Extensions/UITextField+Extension.swift
+++ Dobit/Helpers/Extensions/UITextField+Extension.swift
@@ -11,7 +11,7 @@ extension UITextField {
     func addBottomBorder(){
         let bottomLine = CALayer()
         bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
-        bottomLine.backgroundColor = UIColor.gray.cgColor
+        bottomLine.backgroundColor = UIColor(named: "DoBit Black")!.cgColor
         layer.addSublayer(bottomLine)
         layer.masksToBounds = true
     }
