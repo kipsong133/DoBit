1b22f80138df118f27b0d983fe92ec0ea88d3f25
gusdn5387 <gusdn5387@naver.com>
2022-08-10 15:10:39 +0900

    목표 > 폼 > 색상 선택 시 강조

--- Dobit/UserInterface/Main/Identity/Form/IdentityFormView.swift
+++ Dobit/UserInterface/Main/Identity/Form/IdentityFormView.swift
@@ -12,6 +12,27 @@ class IdentityFormView: UIViewController, BaseViewControllerProtocol, Storyboard
     
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet var colorImageView: [UIImageView]!
+    
+    var selected: UIImageView? {
+        didSet {
+            if selected != nil {
+                oldValue?.isHighlighted = false
+                oldValue?.layer.borderWidth = 0
+            }
+        }
+    }
+    var colors: [String] = [
+        "DoBit Beige",
+        "DoBit Pink",
+        "DoBit Red",
+        "DoBit Yellow",
+        "DoBit Green",
+        "DoBit Skyblue",
+        "DoBit Blue",
+        "DoBit Purple",
+        "DoBit Black"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +52,12 @@ class IdentityFormView: UIViewController, BaseViewControllerProtocol, Storyboard
         
     }
     
+    // MARK: 색상 선택했을 때
+    @objc func colorImageViewDidTap(_ sender: UITapGestureRecognizer) {
+        let selectedView = colorImageView[sender.view!.tag]
+        updateImageView(selectedView)
+    }
+    
     // MARK: - Helpers
     private func setupAttribute() {
         view.backgroundColor = dobitBackgroundColor
@@ -40,7 +67,48 @@ class IdentityFormView: UIViewController, BaseViewControllerProtocol, Storyboard
         [backButton, submitButton].forEach {
             $0.setTitle("", for: .normal)
         }
+        
+        colorImageView.forEach { imageView in
+            let tap = UITapGestureRecognizer(target: self, action: #selector(colorImageViewDidTap(_:)))
+            imageView.addGestureRecognizer(tap)
+            imageView.isUserInteractionEnabled = true
+            
+            imageView.image = UIImage(named: "capsule.template")?.imageWithInsets(insets: UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3), color: colors[imageView.tag])
+            imageView.sizeToFit()
+        }
     }
     
-    // TODO: 색상 선택 시 강조하기
+    // MARK: 색상 선택 시 선택한 색상 강조
+    private func updateImageView(_ imageView: UIImageView?) {
+        guard let imageView = imageView else { return }
+
+        imageView.isHighlighted.toggle()
+        
+        if imageView.isHighlighted {
+            selected = imageView
+            
+            imageView.layer.borderColor = dobitTintColor.cgColor
+            imageView.layer.borderWidth = 1
+            imageView.layer.cornerRadius = imageView.frame.width / 2
+        } else {
+            selected = nil
+            
+            imageView.layer.borderWidth = 0
+        }
+    }
+}
+
+extension UIImage {
+    func imageWithInsets(insets: UIEdgeInsets, color: String) -> UIImage? {
+            UIGraphicsBeginImageContextWithOptions(
+                CGSize(width: self.size.width + insets.left + insets.right,
+                       height: self.size.height + insets.top + insets.bottom), false, self.scale)
+            let context = UIGraphicsGetCurrentContext()
+            context?.setFillColor(UIColor(named: color)!.cgColor)
+            let origin = CGPoint(x: insets.left, y: insets.top)
+            self.draw(at: origin)
+            let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return imageWithInsets
+        }
 }
